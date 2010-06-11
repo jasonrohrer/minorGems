72099c436aee23fa388cb30202fc3f46a9fa8f91
Jason Rohrer <jasonrohrer@fastmail.fm>
2010-06-11 16:56:04 -0600

    Moved GL calls into a central file.

--- game/gameGraphics.h
+++ game/gameGraphics.h
@@ -4,3 +4,24 @@
 
 
 void setDrawColor( double inR, double inG, double inB, double inA );
+
+
+// draw using last set color
+// four vertices per quad
+void drawQuads( int inNumQuads, double inVertices[] );
+
+
+// four r,g,b,a values per quad vertex
+void drawQuads( int inNumQuads, double inVertices[], float inVertexColors[] );
+
+
+// draw using last set color
+// three vertices per triangle
+void drawTriangles( int inNumTriangles, double inVertices[] );
+
+
+// four r,g,b,a values per quad vertex
+void drawTriangles( int inNumTriangles, double inVertices[], 
+                    float inVertexColors[] );
+
+
--- game/platforms/openGL/gameGraphicsGL.cpp
+++ game/platforms/openGL/gameGraphicsGL.cpp
@@ -2,6 +2,71 @@
 
 #include <GL/gl.h>
 
+
+
 void setDrawColor( double inR, double inG, double inB, double inA ) {
     glColor4d( inR, inG, inB, inA );
     }
+
+
+
+void drawQuads( int inNumQuads, double inVertices[] ) {
+    
+    glEnableClientState( GL_VERTEX_ARRAY );
+    glVertexPointer( 2, GL_DOUBLE, 0, inVertices );
+        
+    glDrawArrays( GL_QUADS, 0, inNumQuads * 4 );
+    
+    glDisableClientState( GL_VERTEX_ARRAY );
+    }
+
+
+
+void drawQuads( int inNumQuads, double inVertices[], 
+                float inVertexColors[] ) {
+
+    glEnableClientState( GL_VERTEX_ARRAY );
+    glVertexPointer( 2, GL_DOUBLE, 0, inVertices );
+    
+    
+    glEnableClientState( GL_COLOR_ARRAY );
+    glColorPointer( 4, GL_FLOAT, 0, inVertexColors );
+    
+        
+    glDrawArrays( GL_QUADS, 0, inNumQuads * 4 );
+
+    glDisableClientState( GL_VERTEX_ARRAY );
+    glDisableClientState( GL_COLOR_ARRAY );
+    }
+
+
+
+void drawTriangles( int inNumTriangles, double inVertices[] ) {
+    glEnableClientState( GL_VERTEX_ARRAY );
+    glVertexPointer( 2, GL_DOUBLE, 0, inVertices );
+        
+    glDrawArrays( GL_TRIANGLES, 0, inNumTriangles * 3 );
+    
+    glDisableClientState( GL_VERTEX_ARRAY );
+    }
+
+
+
+void drawTriangles( int inNumTriangles, double inVertices[], 
+                    float inVertexColors[] ) {
+
+    glEnableClientState( GL_VERTEX_ARRAY );
+    glVertexPointer( 2, GL_DOUBLE, 0, inVertices );
+    
+    
+    glEnableClientState( GL_COLOR_ARRAY );
+    glColorPointer( 4, GL_FLOAT, 0, inVertexColors );
+    
+        
+    glDrawArrays( GL_TRIANGLES, 0, inNumTriangles * 3 );
+
+    glDisableClientState( GL_VERTEX_ARRAY );
+    glDisableClientState( GL_COLOR_ARRAY );
+    }
+
+
