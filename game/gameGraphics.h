d59350f9c0f78aaf44ae40b31e7c834c04cdbf46
Jason Rohrer <jasonrohrer@fastmail.fm>
2010-06-11 15:27:39 -0600

    Working on GL-indpendent graphics interface.

---
+++ game/gameGraphics.h
@@ -0,0 +1,6 @@
+
+
+// platform-independent wrapper for graphics primitives
+
+
+void setDrawColor( double inR, double inG, double inB, double inA );
---
+++ game/platforms/openGL/gameGraphicsGL.cpp
@@ -0,0 +1,7 @@
+#include "minorGems/game/gameGraphics.h"
+
+#include <GL/gl.h>
+
+void setDrawColor( double inR, double inG, double inB, double inA ) {
+    glColor4d( inR, inG, inB, inA );
+    }
