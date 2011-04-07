e9b6bcb5b8691d763adcf91c3aa8eb370dd3fc10
Jason Rohrer <jasonrohrer@fastmail.fm>
2011-04-07 12:41:49 -0600

    Added missing header.

---
+++ graphics/rgbaColor.h
@@ -0,0 +1,13 @@
+#ifndef RGBACOLOR_INCLUDED
+#define RGBACOLOR_INCLUDED
+
+
+typedef struct rgbaColor {
+        unsigned char r;
+        unsigned char g;
+        unsigned char b;
+        unsigned char a;
+    } rgbaColor;
+
+
+#endif
