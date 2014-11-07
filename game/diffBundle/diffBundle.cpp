2518568ae94500f7b261a1c5e187a148546fd07d
Jason Rohrer <jasonrohrer@fastmail.fm>
2014-11-07 11:20:55 -0800

    Added diff bundling utility.

---
+++ game/diffBundle/diffBundle.cpp
@@ -0,0 +1,291 @@
+
+
+#include "minorGems/io/file/File.h"
+#include "minorGems/formats/encodingUtils.h"
+
+
+// strips off the first step of a path
+// converts dirA/dirB/test.txt to dirB/test.txt
+static char *getSubdirPath( char *inFullFilePath ) {
+    char delim = Path::getDelimeter();
+    
+    // skip very first, to avoid stopping at root
+    int firstDelimPosition = 1;
+    
+    int found = false;
+    
+    int pathLen = strlen( inFullFilePath );
+    
+    for( int i=0; i<pathLen; i++ ) {
+        if( inFullFilePath[i] == delim ) {
+            firstDelimPosition = i;
+            found = true;
+            break;
+            }
+        }
+
+    if( !found ) {
+        printf( "getSubdirPath didn't find delimeters %c in %s\n",
+                delim, inFullFilePath );
+        
+        return stringDuplicate( inFullFilePath );
+        }
+    return stringDuplicate( &( inFullFilePath[ firstDelimPosition + 1 ] ) );
+    }
+
+
+
+
+static void bundleFiles( File **inFiles, int inNumFiles,
+                         char *inDBZTargetFile ) {
+
+    SimpleVector<unsigned char> fileDataBuffer;
+    
+
+    printf( "Bundling files...\n" );
+    for( int i=0; i<inNumFiles; i++ ) {
+        char *fileName = inFiles[i]->getFullFileName();
+
+        char *fileSubdirName = getSubdirPath( fileName );
+        int size = inFiles[i]->getLength();
+        
+        
+        char *header = autoSprintf( "%d %s %d ",
+                                    strlen( fileSubdirName ),
+                                    fileSubdirName,
+                                    size );
+        fileDataBuffer.appendArray( (unsigned char*)header, strlen( header ) );
+        delete [] header;
+        
+        int contentLength;
+        unsigned char *contents = 
+            inFiles[i]->readFileContents( &contentLength );
+        
+        if( contents != NULL && contentLength == size ) {
+            fileDataBuffer.appendArray( contents, contentLength );
+            }
+        else {
+            printf( "Reading file contents of %s failed, or expected size "
+                    "%d did not match actual size %d\n",
+                    fileName, size, contentLength );
+            }
+        
+        if( contents != NULL ) {
+            delete [] contents;
+            }
+        
+
+                
+        delete [] fileName;
+        
+        delete [] fileSubdirName;
+        }
+    
+    int totalSize = fileDataBuffer.size();
+    
+    unsigned char *data = fileDataBuffer.getElementArray();
+        
+
+    printf( "Compressing bundle...\n" );
+
+    int compSize;
+    unsigned char *compData = zipCompress( data, totalSize, &compSize );
+    
+    if( compData != NULL ) {
+        
+        FILE *outFile = fopen( inDBZTargetFile, "w" );
+        
+        if( outFile != NULL ) {
+            
+            printf( "Writing file %s\n", inDBZTargetFile );
+            
+            fprintf( outFile, "%d %d ", totalSize, compSize );
+            
+            int numWritten = fwrite( compData, 1, compSize, outFile );
+            if( numWritten != compSize ) {
+                printf( "Tried to write %d to file %s, "
+                        "but wrote %d instead\n",
+                        compSize, inDBZTargetFile, numWritten );
+                }
+            fclose( outFile );
+            }
+        else {
+            printf( "Failed to open %s for writing\n", inDBZTargetFile );
+            }
+
+        delete [] compData;
+        }
+    
+
+    delete [] data;
+    }
+
+
+
+
+
+
+// NOTE:
+// this app has a default recursion depth limit of 100 when entering
+// sub directories
+int main( int inNumArgs, char **inArgs ) {
+    
+    if( inNumArgs != 5 ) {
+		printf( "Usage:  diffBundle  dirOld dirNew "
+                "outIncremental.dbz outFull.dbz\n" );
+		return 1;
+		}
+    
+    File dirOld( NULL, inArgs[1] );
+    
+    if( ! dirOld.exists() || ! dirOld.isDirectory() ) {
+        printf( "Directory %s not found\n", inArgs[1] );
+        
+        return 0;
+        }
+    
+    File dirNew( NULL, inArgs[2] );
+
+    if( ! dirNew.exists() || ! dirNew.isDirectory() ) {
+        printf( "Directory %s not found\n", inArgs[2] );
+        
+        return 0;
+        }
+
+    //File outIncFile( NULL, inArgs[3] );
+    //File outFullFile( NULL, inArgs[3] );
+    
+    
+    int numOldChild;
+    File **oldChild = dirOld.getChildFilesRecursive( 100, &numOldChild );
+    
+    int numNewChild;
+    File **newChild = dirNew.getChildFilesRecursive( 100, &numNewChild );
+    
+    // bundle is additive, ignore files in old that are no longer in new
+    
+
+    // first, generate the full DBZ
+    printf( "Making full bundle...\n" );
+    
+    // exclude all dirs
+    SimpleVector<File*> newNonDirs;
+    for( int i=0; i<numNewChild; i++ ) {
+        if( newChild[i]->isDirectory() ) {
+            continue;
+            }
+        newNonDirs.push_back( newChild[i] );
+        }
+    int numNewNonDirs = newNonDirs.size();
+    File **newNonDirsArray = newNonDirs.getElementArray();
+
+    
+    printf( "%d files to bundle\n", numNewNonDirs );
+
+    bundleFiles( newNonDirsArray, numNewNonDirs, inArgs[4] );
+    
+    delete [] newNonDirsArray;
+
+
+    printf( "\n\nMaking incremental bundle...\n" );
+    
+    printf( "Scanning files...\n" );
+    
+    
+    SimpleVector<File*> changedFiles;
+    
+    for( int i=0; i<numNewChild; i++ ) {
+        
+        if( newChild[i]->isDirectory() ) {
+            continue;
+            }
+        
+        char *newFileName = newChild[i]->getFullFileName();
+        
+        char *newFileSubdirName = getSubdirPath( newFileName );
+
+        int newFileLength;
+        unsigned char *newFileContents = 
+            newChild[i]->readFileContents( &newFileLength );
+        
+        char foundOldFile = false;
+        for( int j=0; j<numOldChild && ! foundOldFile; j++ ) {
+            char *oldFileName = oldChild[j]->getFullFileName();
+            
+            char *oldFileSubdirName = getSubdirPath( oldFileName );
+
+            if( strcmp( oldFileSubdirName, newFileSubdirName ) == 0 ) {
+                foundOldFile = true;
+                
+                int oldFileLength;
+                unsigned char *oldFileContents = 
+                    oldChild[j]->readFileContents( &oldFileLength );
+                
+                if( oldFileLength != newFileLength ) {
+                    changedFiles.push_back( newChild[i] );
+                    }
+                else {
+                    for( int b=0; b<newFileLength; b++ ) {
+                        if( oldFileContents[b] != newFileContents[b] ) {
+                            changedFiles.push_back( newChild[i] );
+                            break;
+                            }
+                        }
+                    }
+                
+
+                delete [] oldFileContents;
+                }
+            
+            delete [] oldFileName;
+            delete [] oldFileSubdirName;
+            }
+        
+        if( !foundOldFile ) {
+            changedFiles.push_back( newChild[i] );
+            }
+
+        delete [] newFileName;
+        delete [] newFileSubdirName;
+        delete [] newFileContents;
+        }
+    
+    printf( "%d changed files:\n", changedFiles.size() );
+
+    for( int i=0; i<changedFiles.size(); i++ ) {
+        char *fileName = 
+            (* changedFiles.getElement( i ) )->getFullFileName();
+        
+        char *fileSubdirName = getSubdirPath( fileName );
+
+        printf( "  %s\n", fileSubdirName );
+
+        delete [] fileName;
+        delete [] fileSubdirName;
+        }
+    
+    
+    int numChanged = changedFiles.size();
+    File **changedFilesArray = changedFiles.getElementArray();
+
+    bundleFiles( changedFilesArray, numChanged, inArgs[3] );
+
+    delete [] changedFilesArray;
+
+    
+    for( int i=0; i<numOldChild; i++ ) {
+        delete oldChild[i];
+        }
+    delete [] oldChild;
+
+    
+    for( int i=0; i<numNewChild; i++ ) {
+        delete newChild[i];
+        }
+    delete [] newChild;
+
+    
+
+    
+    return 1;
+    }
