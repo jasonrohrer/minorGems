b662f305d123dfafce809c9e18124404ce2641ce
Jason Rohrer <jasonrohrer@fastmail.fm>
2015-06-29 12:00:16 -0700

    Added support for reading PNG files using libpng.

--- graphics/converters/PNGImageConverter.cpp
+++ graphics/converters/PNGImageConverter.cpp
@@ -639,10 +639,154 @@ void PNGImageConverter::formatImage( Image *inImage,
 
 
 
+
+
+void libpngReadCallback( png_structp png_ptr,
+                         png_bytep data, png_size_t length ) {
+    
+    // unpack our extra parameter
+    void *read_io_ptr = png_get_io_ptr( png_ptr );
+
+    InputStream *inStream = (InputStream *)read_io_ptr;    
+    
+    inStream->read( data, length );
+    }
+
+
+
+
 Image *PNGImageConverter::deformatImage( InputStream *inStream ) {
 
-    printf( "ERROR:  reading PNG images is not supported by "
-            "PNGImageConverter\n" );
+    unsigned char header[8];
+    
+    inStream->read( header, 8 );
+    
+    if( png_sig_cmp( header, 0, 8 ) ) {
+        printf( "Stream does not contain a valid PNG header\n" );
+        return NULL;
+        }
+    
+    png_structp png_ptr = png_create_read_struct( PNG_LIBPNG_VER_STRING,
+                                                  NULL, NULL, NULL );
+    
+    
+    if( !png_ptr ) {
+        printf( "PNG Reading:  png_create_read_struct failed\n" );
+        return NULL;
+        }
+    
 
-    return NULL;
+	png_infop info_ptr = png_create_info_struct( png_ptr );
+	
+    if( !info_ptr ) {
+        png_destroy_read_struct( &png_ptr, NULL, NULL );
+
+        printf( "PNG Reading:  png_create_info_struct failed\n" );
+        return NULL;
+        }
+    
+    // weird way that libpng handles errors with a jump
+	if( setjmp( png_jmpbuf( png_ptr ) ) ) {
+        png_destroy_read_struct( &png_ptr, &info_ptr, NULL );
+
+        printf( "PNG Reading: error when setting read funciton\n" );
+        return NULL;
+        }
+
+    png_set_read_fn( png_ptr,
+                     (void*)inStream, 
+                     libpngReadCallback );
+
+    
+    png_set_sig_bytes( png_ptr, 8 );
+    
+    png_read_info( png_ptr, info_ptr );
+    
+    int w = png_get_image_width( png_ptr, info_ptr );
+    int h = png_get_image_height( png_ptr, info_ptr );
+    png_byte color_type = png_get_color_type( png_ptr, info_ptr );
+    png_byte bit_depth = png_get_bit_depth( png_ptr, info_ptr );
+    
+    printf( "color type = %d bit depth = %d\n", color_type, bit_depth );
+    
+    if( color_type == PNG_COLOR_TYPE_PALETTE ) {
+        png_set_palette_to_rgb( png_ptr );
+        }
+    if( color_type == PNG_COLOR_TYPE_GRAY ) {
+        png_set_gray_to_rgb( png_ptr );
+        }
+    if( png_get_valid( png_ptr, info_ptr, PNG_INFO_tRNS ) ) {
+        png_set_tRNS_to_alpha( png_ptr );
+        }
+    if( color_type == PNG_COLOR_TYPE_RGB ) {
+        png_set_add_alpha( png_ptr, 0xFF, PNG_FILLER_AFTER );
+        }
+    
+    if( bit_depth == 16 ) {
+        png_set_strip_16( png_ptr );
+        }
+    
+    if( bit_depth < 8 ) {
+        png_set_packing( png_ptr );
+        }
+    
+
+    png_read_update_info( png_ptr, info_ptr );
+    
+
+    
+    png_bytep *rowPtrs = new png_bytep[ h ];
+    
+    // libpng converting it to 8-bit-per-channel RGBA
+    unsigned char *raster = new unsigned char[ w * h * 4 ];
+    
+    memset( raster, 0, w * h * 4 );
+    
+    for( int y=0; y<h; y++ ) {
+        
+        rowPtrs[y] = (png_bytep) &raster[ y * w * 4 ];
+        }
+    
+    
+    if( setjmp( png_jmpbuf( png_ptr ) ) ) {
+        delete [] raster;
+        delete [] rowPtrs;
+        png_destroy_read_struct( &png_ptr, &info_ptr, NULL );
+
+        printf( "PNG Reading: error reading image bytes\n" );
+        return NULL;
+        }
+
+    png_read_image( png_ptr, rowPtrs );
+
+
+    png_destroy_read_struct( &png_ptr, &info_ptr, NULL );
+    
+
+    Image *image = new Image( w, h, 4, false );
+
+	double *red = image->getChannel( 0 );
+	double *green = image->getChannel( 1 );
+	double *blue = image->getChannel( 2 );
+    double *alpha = image->getChannel( 3 );
+	
+	long rasterIndex = 0;
+	double inv255 = 1.0 / 255.0;
+
+    int numPixels = w * h;
+    
+    for( int i=0; i<numPixels; i++ ) {
+        // optimization:  use postfix increment operators in
+        // array index
+        // (found with profiler)
+        red[i] = inv255 * raster[ rasterIndex ++ ];
+        green[i] = inv255 * raster[ rasterIndex ++ ];
+        blue[i] = inv255 * raster[ rasterIndex ++ ];
+        alpha[i] = inv255 * raster[ rasterIndex ++ ];
+        }
+    
+    delete [] raster;
+    delete [] rowPtrs;
+
+    return image;
 	}
