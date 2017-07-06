de87d3c7344b92fdcf2e94578a40fdd2a9927fef
Jason Rohrer <jasonrohrer@fastmail.fm>
2017-07-06 10:33:29 -0700

    Added new no-scaling code for post win 8.1.

--- game/platforms/SDL/gameSDL.cpp
+++ game/platforms/SDL/gameSDL.cpp
@@ -1369,19 +1369,71 @@ static char isSettingsFolderFound() {
 int mainFunction( int inNumArgs, char **inArgs ) {
 
 #ifdef WIN32
-    // backwards compatible code
-    // on vista and higher, this will tell Windows that we are DPI aware
-    // and that we should not be artificially scaled.
-    //
-    // Found here:  http://www.rw-designer.com/DPI-aware
-    HMODULE hUser32 = LoadLibrary( _T( "user32.dll" ) );
-    typedef BOOL (*SetProcessDPIAwareFunc)();
-    SetProcessDPIAwareFunc setDPIAware = 
-        (SetProcessDPIAwareFunc)GetProcAddress( hUser32, "SetProcessDPIAware" );
-    if( setDPIAware ) {
-        setDPIAware();
+
+
+    HMODULE hShcore = LoadLibrary( _T( "shcore.dll" ) );
+
+    if( hShcore != NULL ) {
+        printf( "shcore.dll loaded successfully\n" );
+        
+        typedef enum _PROCESS_DPI_AWARENESS { 
+            PROCESS_DPI_UNAWARE            = 0,
+            PROCESS_SYSTEM_DPI_AWARE       = 1,
+            PROCESS_PER_MONITOR_DPI_AWARE  = 2
+            } PROCESS_DPI_AWARENESS;
+        
+        typedef HRESULT (*SetProcessDPIAwarenessFunc)( PROCESS_DPI_AWARENESS );
+        
+        SetProcessDPIAwarenessFunc setAwareness = 
+            (SetProcessDPIAwarenessFunc)GetProcAddress(
+                hShcore, 
+                "SetProcessDPIAwareness" );
+        
+        if( setAwareness ) {
+            printf( "Found SetProcessDPIAwareness function in shcore.dll\n" );
+            setAwareness( PROCESS_PER_MONITOR_DPI_AWARE );
+            }
+        else {
+            printf( "Could NOT find SetProcessDPIAwareness function in "
+                    "Shcore.dll\n" );
+            }
+        FreeLibrary( hShcore );
         }
-    FreeLibrary( hUser32 );
+    else {
+        printf( "shcore.dll NOT loaded successfully... pre Win 8.1 ?\n" );
+
+
+        printf( "   Trying to load user32.dll instead\n" );
+        
+
+        // backwards compatible code
+        // on vista and higher, this will tell Windows that we are DPI aware
+        // and that we should not be artificially scaled.
+        //
+        // Found here:  http://www.rw-designer.com/DPI-aware
+        HMODULE hUser32 = LoadLibrary( _T( "user32.dll" ) );
+        
+        if( hUser32 != NULL ) {
+            
+            typedef BOOL (*SetProcessDPIAwareFunc)();
+            
+            SetProcessDPIAwareFunc setDPIAware = 
+                (SetProcessDPIAwareFunc)GetProcAddress( hUser32, 
+                                                        "SetProcessDPIAware" );
+            if( setDPIAware ) {
+                printf( "Found SetProcessDPIAware function in user32.dll\n" );
+                setDPIAware();
+                }
+            else {
+                printf( "Could NOT find SetProcessDPIAware function in "
+                        "user32.dll\n" );
+                }
+            FreeLibrary( hUser32 );
+            }
+        else {
+            printf( "Failed to load user32.dll\n" );
+            }
+        }
 #endif
 
 
