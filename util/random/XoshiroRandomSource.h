e87727d8660a2d1ea5a32381c1658f2f10b110f5
Jason Rohrer <jasonrohrer@fastmail.fm>
2025-01-10 19:46:05 -0500

    Better seeding with splitmix32

--- util/random/XoshiroRandomSource.h
+++ util/random/XoshiroRandomSource.h
@@ -90,31 +90,70 @@ inline XoshiroRandomSource::XoshiroRandomSource( unsigned int inSeedA ) {
 
         
 
+// the authors of Xoshiro suggest using splitmix64 to create good seeds
+//
+// I've noticed that if seeding with low numbers, the first few outputs of
+// Xoshiro are 0 or low, which isn't great.
+//
+// Thus, I think that fluffing up seeds is a good idea.
+//
+// This is, in part, because Xoshiro has no noisy constants in it, so
+// the first values aren't very noisy, if the seed has mostly 0 bits.
+//
+// In our case, we need 4 32-bit seeds, so I'm using splitmix32, which I found
+// here:
+//
+// https://stackoverflow.com/questions/17035441/looking-for-decent-quality-prng-with-only-32-bits-of-state
+//
+// There's also a bad edge case when Xoshiro is seeded with all-zero values
+// which might happen, depending on the application (if seeing with world
+// position x,y, for example)
+//
+// Generating seeds with splitmix32 based on the provided seeds also handles
+// this edge case.
+//
+
+static uint32_t splitmix32( uint32_t *inState ) {
+    uint32_t z = ( *inState += 0x9e3779b9 );
+    z ^= z >> 16; z *= 0x21f0aaad;
+    z ^= z >> 15; z *= 0x735a2d97;
+    z ^= z >> 15;
+    return z;
+    }
+
+
+
 void XoshiroRandomSource::reseed( unsigned int inSeedA,
                                   unsigned int inSeedB,
                                   unsigned int inSeedC,
                                   unsigned int inSeedD ) {
+    uint32_t splitMixState = inSeedA;
     
-    if( inSeedA == 0 && 
-        inSeedB == 0 &&
-        inSeedC == 0 &&
-        inSeedD == 0 ) {
-        
-        // edge case, state is 0 everywhere, which breaks xoshiro
-        // force-fix it
-        inSeedA = 1;
-        }
+    mState[0] = splitmix32( &splitMixState );
+
     
-    mState[0] = inSeedA;
-    mState[1] = inSeedB;
-    mState[2] = inSeedC;
-    mState[3] = inSeedD;
+    splitMixState += inSeedB;
+
+    mState[1] = splitmix32( &splitMixState );
+
+
+    splitMixState += inSeedC;
+
+    mState[2] = splitmix32( &splitMixState );
+
+
+    splitMixState += inSeedD;
+
+    mState[3] = splitmix32( &splitMixState );
     }
 
 
 
 void XoshiroRandomSource::reseed( unsigned int inSeed ) {
-    reseed( inSeed, inSeed, inSeed, inSeed );
+    // if using a single seed, specify 0 for the other ones
+    // this allows us to generate a sequence of 4 values directly from
+    // splitmix32 above
+    reseed( inSeed, 0, 0, 0 );
     }
 
 
